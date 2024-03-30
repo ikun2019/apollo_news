732dc77d7f8f09a42505cd79d7090f3057915b49
ikun <0215.miyatto@gmail.com>
2024-03-30 21:14:43 +0900

    linksの追加

--- api/src/graphql/resolvers.js
+++ api/src/graphql/resolvers.js
@@ -1,5 +1,14 @@
+let links = [
+  {
+    id: 'link-0',
+    description: 'GraphQLチュートリアル',
+    url: 'https://google.com'
+  }
+]
+
 export const resolvers = {
   Query: {
-    info: () => 'Hello World!'
+    info: () => 'Hello World!',
+    links: () => links
   }
 }
