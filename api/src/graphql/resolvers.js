3830e5f8bb11bd4f971820382696b15795afb8fb
ikun <0215.miyatto@gmail.com>
2024-03-30 21:56:21 +0900

    mutation postの作成

--- api/src/graphql/resolvers.js
+++ api/src/graphql/resolvers.js
@@ -1,14 +1,22 @@
-let links = [
-  {
-    id: 'link-0',
-    description: 'GraphQLチュートリアル',
-    url: 'https://google.com'
-  }
-]
+let links = [];
 
 export const resolvers = {
   Query: {
     info: () => 'Hello World!',
     links: () => links
+  },
+  Mutation: {
+    post: (parent, args, context) => {
+      const newId = links.length + 1;
+
+      const link = {
+        id: `link-${newId}`,
+        description: args.input.description,
+        url: args.input.url
+      };
+
+      links.push(link);
+      return link;
+    }
   }
 }
